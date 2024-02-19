7e90fbe4d3438009ca5ceeabe0603c40a6b2f9f4
a1exprod <alexander.schnabl@outlook.com>
2024-02-19 10:43:41 +0100

    Routing integration v 2.0

--- src/navigation/Navbar.js
+++ src/navigation/Navbar.js
@@ -1,14 +1,15 @@
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <>
             <nav className="mainNav">
-                <a href="/"><img class="nav-logo" src="./mediacontent/logos/F1-white.svg"/></a>
+                <Link to="/home-of-f1"><img class="nav-logo" src="./mediacontent/logos/F1-white.svg"/></Link>
                 <ul>
 
                     <li>
-                    <a className="nav" href="/">Home</a>
-<a className="nav" href="/schedule">Schedule</a>
+                        <Link to="/home-of-f1" className="nav">Home</Link>
+                        <Link to="/home-of-f1/pages/schedule" className="nav">Schedule</Link>
                     </li>
                 </ul>
             </nav>
