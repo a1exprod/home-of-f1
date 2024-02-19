4e269be99f17fc004f155f644135af29c32deb4b
a1exprod <alexander.schnabl@outlook.com>
2024-02-19 14:27:19 +0100

    Image Handling v1.7

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -16,7 +16,7 @@ export default function Home() {
     console.log("Renderd Countodwn in home");
     return (
         <>
-            <h1>Home of Formula 1 HH</h1>
+            <h1>Home of Formula 1 44</h1>
             <CountdownNextEvent />
             
 
